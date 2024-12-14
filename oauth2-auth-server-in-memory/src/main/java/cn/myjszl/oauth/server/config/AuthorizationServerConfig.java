a13b7a70289fff2b7316d08de8b49dee7b69f223
George <george_95@126.com>
2024-12-14 15:27:26 +0800

    添加资源服务相关注释

--- oauth2-auth-server-in-memory/src/main/java/cn/myjszl/oauth/server/config/AuthorizationServerConfig.java
+++ oauth2-auth-server-in-memory/src/main/java/cn/myjszl/oauth/server/config/AuthorizationServerConfig.java
@@ -104,10 +104,10 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
      * spring Security框架默认的访问端点有如下6个：
      *      - /oauth/authorize：获取授权码的端点
      *      - /oauth/token：获取令牌端点
-     *  - /oauth/confifirm_access：用户确认授权提交端点
-     *  - /oauth/error：授权服务错误信息端点
-     *  - /oauth/check_token：用于资源服务访问的令牌解析端点
-     *  - /oauth/token_key：提供公有密匙的端点，如果你使用JWT令牌的话
+     *      - /oauth/confifirm_access：用户确认授权提交端点
+     *      - /oauth/error：授权服务错误信息端点
+     *      - /oauth/check_token：用于资源服务访问的令牌解析端点
+     *      - /oauth/token_key：提供公有密匙的端点，如果你使用JWT令牌的话
      *
      *  当然如果业务要求需要改变这些默认的端点的url，也是可以修改的，AuthorizationServerEndpointsConfigurer有一个方法，如下：
      *      - public AuthorizationServerEndpointsConfigurer pathMapping(String defaultPath, String customPath)
